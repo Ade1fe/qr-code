02a041baf18ab03d6c6121b8f1f29879e1626306
Ade1fe <addypearl09@gmail.com>
2025-08-08 15:49:17 +0100

    added img and new vid

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-const videoPageURL = "https://video-qr-demo.netlify.app/watch.html"; // Replace with your actual Netlify URL
+const videoPageURL = "https://video-qr-demo.netlify.app/watch.html"; 
 
 new QRCode(document.getElementById("qrcode"), {
   text: videoPageURL,
   width: 256,
   height: 256,
-  colorDark: "#000000",       // Blue
+  colorDark: "#000000",       
   colorLight: "#F0F0F0",      // Light background
   correctLevel: QRCode.CorrectLevel.H
 });
